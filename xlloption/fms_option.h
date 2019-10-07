e5d9ea0bc1af196f4cba26e7ab266d181667c967
Keith A. Lewis <keithalewis@live.com>
2019-10-07 00:21:54 -0400

    Edge cases for moneyness

--- xlloption/fms_option.h
+++ xlloption/fms_option.h
@@ -2,7 +2,9 @@
 // F = f exp(sX - kappa(s))
 // F <= k iff X <= (kappa(s) + log k/f)/s
 #pragma once
+#include <algorithm>
 #include <functional>
+#include <stdexcept>
 #include <tuple>
 #include "fms_normal.h"
 #include "fms_Bell.h"
@@ -12,10 +14,35 @@
 
 namespace fms::option {
 
+    // F <= k iff X <= z where F = f exp(sX - kappa(s)) and z is the moneyness z = (kappa(s) + log(k/f))/s
     template<class F, class S, class K, class Kappa = std::function<S(S)>>
     inline auto moneyness(F f, S s, K k, const Kappa& kappa)
     {
-        //!!! check 
+        using Z = decltype((kappa(s) + log(k / f)) / s);
+        auto scale = std::max(f, std::max(s, k));
+
+        if (f < 0) {
+            throw std::invalid_argument("fms::option::moneyness: the forward must be non-negative");
+        }
+        if (k < 0) {
+            throw std::invalid_argument("fms::option::moneyness: the strike must be non-negative");
+        }
+        if (s < 0) {
+            throw std::invalid_argument("fms::option::moneyness: the variance must be non-negative");
+        }
+        // Edge cases
+        if (f + F(1) == F(scale)) { // f = 0 to machine epsilon
+            return std::numeric_limits<Z>::infinity();
+        }
+        if (k + K(1) == K(scale)) {
+            return -std::numeric_limits<Z>::infinity();
+        }
+        if (s + S(1) == S(scale)) {
+            // k/f < 1 -> -infinity
+            // k/f > 1 -> ininity
+            return copysign(Z(1), k - f) * std::numeric_limits<Z>::infinity();
+        }
+       
         return (kappa(s) + log(k / f)) / s;
     }
 
@@ -29,8 +56,7 @@ namespace fms::option {
         using fms::sequence::list;
         using fms::sequence::skip;
         using fms::sequence::sum;
-        using fms::sequence::take;
-
+ 
         auto [mu, sigma, kappa3] = cumulant::normalize(kappa);
         auto x_ = (x - mu) / sigma;
 
@@ -39,7 +65,7 @@ namespace fms::option {
         bell b(concatenate(list({ 0, 0 }), kappa3)); // reduced Bell polynomial
         auto b3 = skip(3, b);
 
-        auto s = sum(take(100,epsilon(constant(normal::pdf(x_)) * b3 * H2)));
+        auto s = sum(epsilon(constant(normal::pdf(x_)) * b3 * H2));
 
         return normal::cdf(x_) - s;
     }
