5b153e7ea8019c3d89fc8a98ddf83341227bcd04
Keith A. Lewis <keithalewis@live.com>
2019-10-09 00:10:41 -0400

    Factor out normal probability

---
+++ xlloption/fms_probability_normal.t.cpp
@@ -0,0 +1,13 @@
+// fms_probability_normal.t.cpp - Test Normal probability distribution.
+#include <cassert>
+#include "fms_probability_normal.h"
+
+using namespace fms::probability;
+
+template<class X>
+int test_fms_probability_normal()
+{
+    return 0;
+}
+
+int test_fms_probability_normal_double = test_fms_probability_normal<double>();
